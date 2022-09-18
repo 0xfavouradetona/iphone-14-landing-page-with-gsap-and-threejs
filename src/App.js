5d757dd087086596eb94ed9cd5cd0f475de0c60a
0xfavouradetona <favouradetona7@gmail.com>
2022-09-18 05:46:15 -0400

    src

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import BatterySection from "./sections/BatterySection";
+import ColorSection from "./sections/ColorSection";
+import DesignSection from "./sections/DesignSection";
+import DisplaySection from "./sections/DisplaySection";
+import HeroSection from "./sections/HeroSection";
+import PhoneModel from "./sections/PhoneModel";
+import ProcessorSection from "./sections/ProcessorSection";
+import Quote from "./sections/Quote";
+import { GlobalStyle } from "./styles/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Quote />
+      <PhoneModel />
+      <HeroSection />
+      <DesignSection />
+      <DisplaySection />
+      <ProcessorSection />
+      <BatterySection />
+      <ColorSection />
+    </>
+  );
+}
+
+export default App;
